46cc3a6eb6cf2124b2c9d9127b63f1b79b89f5fd
NexusSource <55806025+NexusSource@users.noreply.github.com>
2021-11-04 05:57:37 -0400

    Implemented Text Store Service

--- src/Services/TextStoreService.cs
+++ src/Services/TextStoreService.cs
@@ -1,37 +1,89 @@
-﻿using Refor.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using NodaTime;
+using Refor.Models;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Refor.Services
 {
     public interface ITextStoreService
     {
-        Task<StoredText> Get(string id);
-        Task<StoredText> Upload(string text);
-        Task<bool> Delete(StoredText storedText);
+        ValueTask<bool> Delete(StoredText storedText);
+        ValueTask<StoredText?> Get(string id);
         IAsyncEnumerable<StoredText> Get(int page);
+        ValueTask<StoredText> Upload(string text);
     }
 
     public class TextStoreService : ITextStoreService
     {
-        public Task<bool> Delete(StoredText storedText)
+        private readonly IClock _clock;
+        private readonly ILogger _logger;
+        private readonly ReforContext _reforContext;
+        private readonly IRandomStringService _randomStringService;
+
+        public TextStoreService(IClock clock, ILogger<TextStoreService> logger, ReforContext reforContext, IRandomStringService randomStringService)
         {
-            throw new System.NotImplementedException();
+            _clock = clock;
+            _logger = logger;
+            _reforContext = reforContext;
+            _randomStringService = randomStringService;
         }
 
-        public Task<StoredText> Get(string id)
+        public async ValueTask<bool> Delete(StoredText storedText)
         {
-            throw new System.NotImplementedException();
+            _logger.LogInformation("Attempting to delete stored text with the ID '{ID}'.", storedText.ID);
+            StoredText? internalFetch = await _reforContext.Texts.FindAsync(storedText.ID);
+            if (internalFetch is null)
+            {
+                _logger.LogWarning("We tried to delete a stored text which doesn't exist in the database.");
+                return false;
+            }
+            _logger.LogInformation("Found the stored text we want to delete. Removing it from the database.");
+            _reforContext.Texts.Remove(internalFetch);
+            await _reforContext.SaveChangesAsync();
+            return true;
+        }
+
+        public ValueTask<StoredText?> Get(string id)
+        {
+            return _reforContext.Texts.FindAsync(id)!;
         }
 
         public IAsyncEnumerable<StoredText> Get(int page)
         {
-            throw new System.NotImplementedException();
+            return _reforContext.Texts.Skip(10 * page).Take(10).AsAsyncEnumerable();
         }
 
-        public Task<StoredText> Upload(string text)
+        public async ValueTask<StoredText> Upload(string text)
         {
-            throw new System.NotImplementedException();
+            _logger.LogInformation("Upload a new stored text.");
+            _logger.LogInformation("Looking for a new unique key.");
+
+            string? id = null;
+            while (id is null)
+            {
+                // We retry until we have ID. The RandomStringService will very rarely give a duplicate.
+                // but, there's actually no true guarantee.
+                string potentialID = _randomStringService.GetRandomString();
+                StoredText? potentialDuplicateIDText = await Get(potentialID);
+                if (potentialDuplicateIDText is null)
+                    id = potentialID;
+            }
+            _logger.LogInformation("We have acquired an ID '{ID}'.", id);
+
+            StoredText storedText = new()
+            {
+                ID = id,
+                Text = text,
+                Uploaded = _clock.GetCurrentInstant()
+            };
+
+            _reforContext.Texts.Add(storedText);
+            _logger.LogInformation("Saving new stored text to the database.");
+            await _reforContext.SaveChangesAsync();
+            return storedText;
         }
     }
 }
--- src/Startup.cs
+++ src/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using NodaTime;
 using Refor.Services;
 using System.Security.Cryptography;
 
@@ -23,9 +24,10 @@ namespace Refor
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSingleton<IRandomStringService, RandomStringService>();
             services.AddSingleton<RandomNumberGenerator, RNGCryptoServiceProvider>();
+            services.AddSingleton<IRandomStringService, RandomStringService>();
             services.AddScoped<ITextStoreService, TextStoreService>();
+            services.AddSingleton<IClock>(SystemClock.Instance);
 
             services.AddDbContext<ReforContext>(options =>
             {
